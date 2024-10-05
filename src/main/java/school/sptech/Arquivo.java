540974fd9462d087644de97d4e2eea5ffc8eba34
GustavoLuzz <gustavo.barros@sptech.school>
2024-10-05 01:08:53 -0300

    adicionando a leitura dos dados da tabela, porém o método carregarArquivo está apenas testando exibir os dados da tabela(versão incorreta)

--- src/main/java/school/sptech/Arquivo.java
+++ src/main/java/school/sptech/Arquivo.java
@@ -2,6 +2,9 @@ package school.sptech;
 
 
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -14,23 +17,44 @@ public class Arquivo {
 
     private final BaseDeDados baseDeDados = new BaseDeDados();
 
-    private Path caminho = Path.of("C:/Users/Gusta/Downloads/base-de-dados.xlsx");
+    private final Path caminho = Path.of("C:\\Users\\Gusta\\Downloads\\base-de-dados.xlsx");
 
     public Arquivo() {
         try {
             carregarArquivo();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void carregarArquivo() throws IOException {
 
-        try (InputStream arquivo = Files.newInputStream(caminho);
-             Workbook workbook = new XSSFWorkbook(arquivo)) {
+        InputStream arquivo = Files.newInputStream(caminho);
+        Workbook workbook = new XSSFWorkbook(arquivo);
 
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
+        Sheet sheet = workbook.getSheetAt(0);
+
+
+        for (Row linha : sheet) {
+
+
+            for (Cell celula : linha) {
+                switch (celula.getCellType()) {
+                    case STRING:
+                        System.out.print(celula.getStringCellValue() + "\t");
+                        break;
+                    case NUMERIC:
+                        System.out.print(celula.getNumericCellValue() + "\t");
+                        break;
+                    case BOOLEAN:
+                        System.out.print(celula.getBooleanCellValue() + "\t");
+                        break;
+                    default:
+                        System.out.print("Tipo desconhecido\t");
+                }
+            }
+            System.out.println(); // Nova linha após cada linha da planilha
         }
     }
 
