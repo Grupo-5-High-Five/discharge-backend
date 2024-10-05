f5c69207927dc75207f8896c11dec26d26b3e10a
GustavoLuzz <gustavo.barros@sptech.school>
2024-10-04 23:35:35 -0300

    Melhorando a estrutura do código, removendo funcoes da classe main e inserindo-as na S3Provider

--- src/main/java/school/sptech/S3Provider.java
+++ src/main/java/school/sptech/S3Provider.java
@@ -1,11 +1,22 @@
 package school.sptech;
 
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
 
 public class S3Provider {
+
     private final AwsSessionCredentials credentials;
+    private String caminhoParaInstalacao = "C:/Users/Gusta/Downloads/base-de-dados.xlsx";
 
     private final String bucketName = "discharge-bucket";
 
@@ -24,7 +35,32 @@ public class S3Provider {
                 .build();
     }
 
+    public String getCaminhoParaInstalacao() {
+        return caminhoParaInstalacao;
+    }
 
+    public S3Object buscarbaseDeDados() {
 
+        ListObjectsRequest listObjects = ListObjectsRequest.builder()
+                .bucket("discharge-bucket")
+                .build();
+        List<S3Object> objects = getS3Client().listObjects(listObjects).contents();
 
+        // Define o arquivo da base de dados como o primeiro da lista e retorna
+        return objects.getFirst();
+    }
+
+    public void instalarBaseDeDados(){
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("discharge-bucket")
+                .key(buscarbaseDeDados().key())
+                .build();
+
+        try {
+            getS3Client().getObject(getObjectRequest, Paths.get(caminhoParaInstalacao));
+        }
+        catch (SdkClientException e) {
+            throw new RuntimeException("Arquivo já instalado no diretório!");
+        }
+    }
 }
