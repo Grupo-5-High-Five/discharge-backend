1406ab8ba8ee5dd39cf478283741986dbd5402ef
GustavoLuzz <gustavo.barros@sptech.school>
2024-11-09 17:02:16 -0300

    Adicionando exceção de Input e Output ao método Main

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -1,27 +1,35 @@
 package school.sptech;
 
+import com.slack.api.methods.SlackApiException;
 import org.apache.log4j.PropertyConfigurator;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws SlackApiException, IOException {
 
         String log4jConfPath = "target/log4j.properties";
         PropertyConfigurator.configure(log4jConfPath);
+//
+//        //Instancia uma nova base de dados armazenada em um bucket S3
+//        System.out.println("Iniciando conexão com a AWS e com o bucket S3...\n");
+//        BaseDeDados baseDeDados = new BaseDeDados();
+//
+//        // Busca a base de dados dentro do bucket.
+//        System.out.println("Buscando a base de dados dentro do bucket selecionado...");
+//        baseDeDados.getBaseDeDados();
+//
+//        //Instância um novo arquivo (Base de dados em formato de tabela, tratada, pronta para inserção no banco)
+//        Arquivo arquivo = new Arquivo();
+//
+//        //Inserindo o total de 480 leituras, 5 dias fiscais
+//        arquivo.inserirLeiturasNoBanco();
+//        System.out.println("Dia de leituras inserido no banco de dados com sucesso!");
 
-        //Instancia uma nova base de dados armazenada em um bucket S3
-        System.out.println("Iniciando conexão com a AWS e com o bucket S3...\n");
-        BaseDeDados baseDeDados = new BaseDeDados();
+        Mensagem mensagem = new Mensagem();
 
-        // Busca a base de dados dentro do bucket.
-        System.out.println("Buscando a base de dados dentro do bucket selecionado...");
-        baseDeDados.getBaseDeDados();
+        mensagem.enviarMensagem();
 
-        //Instância um novo arquivo (Base de dados em formato de tabela, tratada, pronta para inserção no banco)
-        Arquivo arquivo = new Arquivo();
-
-        //Inserindo o total de 480 leituras, 5 dias fiscais
-        arquivo.inserirLeiturasNoBanco();
-        System.out.println("Dia de leituras inserido no banco de dados com sucesso!");
     }
 }
