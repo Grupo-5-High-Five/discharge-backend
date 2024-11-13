c02756050bb0f817d1a9b089924ff67b5aa17c8c
Isaias-Oliveira <isaiasoliveirabjj@gmail.com>
2024-11-13 20:42:02 -0300

    Subindo método para salvar a mensagem do slack no banco

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -9,23 +9,23 @@ public class Main {
 
     public static void main(String[] args) throws SlackApiException, IOException {
 
-//        String log4jConfPath = "target/log4j.properties";
-//        PropertyConfigurator.configure(log4jConfPath);
-//
-//        //Instancia uma nova base de dados armazenada em um bucket S3
-//        System.out.println("Iniciando conexão com a AWS e com o bucket S3...\n");
-//        BaseDeDados baseDeDados = new BaseDeDados();
-//
-//        // Busca a base de dados dentro do bucket.
-//        System.out.println("Buscando a base de dados dentro do bucket selecionado...");
-//        baseDeDados.getBaseDeDados();
-//
-//        //Instância um novo arquivo (Base de dados em formato de tabela, tratada, pronta para inserção no banco)
-//        Arquivo arquivo = new Arquivo();
-//
-//        //Inserindo o total de 480 leituras, 5 dias fiscais
-//        arquivo.inserirLeiturasNoBanco();
-//        System.out.println("Dia de leituras inserido no banco de dados com sucesso!");
+        String log4jConfPath = "target/log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
+
+        //Instancia uma nova base de dados armazenada em um bucket S3
+        System.out.println("Iniciando conexão com a AWS e com o bucket S3...\n");
+        BaseDeDados baseDeDados = new BaseDeDados();
+
+        // Busca a base de dados dentro do bucket.
+        System.out.println("Buscando a base de dados dentro do bucket selecionado...");
+        baseDeDados.getBaseDeDados();
+
+        //Instância um novo arquivo (Base de dados em formato de tabela, tratada, pronta para inserção no banco)
+        Arquivo arquivo = new Arquivo();
+
+        //Inserindo o total de 480 leituras, 5 dias fiscais
+        arquivo.inserirLeiturasNoBanco();
+        System.out.println("Dia de leituras inserido no banco de dados com sucesso!");
 
         Mensagem mensagem = new Mensagem();
 
