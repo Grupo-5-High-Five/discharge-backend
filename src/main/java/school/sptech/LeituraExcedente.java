4f825ab40e204e212be608b68c556cc7544b3ecb
gustavoluzz <gustavo.barros@sptechschool>
2024-11-19 19:40:33 -0300

    Criação da classe LeituraExcedente

---
+++ src/main/java/school/sptech/LeituraExcedente.java
@@ -0,0 +1,44 @@
+package school.sptech;
+
+public class LeituraExcedente extends Leitura {
+
+
+    static final Double LIMITE_CONSUMO = 100.00;
+    static final Double LIMITE_EMISSAO = 1.00;
+
+
+    public LeituraExcedente(
+            String data, Double consumo, Double potenciaReativaAtrasada,
+            Double potenciaReativaAdiantada, Double emissao,
+            Double fatorPotenciaAtrasado, Double fatorPotenciaAdiantado, String statusSemana, String diaSemana
+    ) {
+        super(
+                data, consumo, potenciaReativaAtrasada,
+                potenciaReativaAdiantada, emissao, fatorPotenciaAtrasado,
+                fatorPotenciaAdiantado, statusSemana, diaSemana
+        );
+    }
+
+
+    public LeituraExcedente() {
+
+    }
+
+
+    public static Double getLimiteConsumo() {
+        return LIMITE_CONSUMO;
+    }
+
+    public static Double getLimiteEmissao() {
+        return LIMITE_EMISSAO;
+    }
+
+
+    public Double consumoExcedente() {
+        return LIMITE_CONSUMO - getConsumo();
+    }
+
+    public Double emissaoExcedente() {
+        return LIMITE_EMISSAO - getEmissao();
+    }
+}
